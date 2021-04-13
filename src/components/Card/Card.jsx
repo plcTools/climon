9f27900e45d207075cae8732b47c210502cf70a4
Patricio Cadenas <plcTools@users.noreply.github.com>
2021-04-13 10:59:01 -0300

    css card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,12 +8,15 @@ export default function Card({ min, max, name, img, onClose, id }) {
       <div id="closeIcon" className="row">
         <button onClick={onClose} className="btn btn-sm btn-danger">X</button>
       </div>
+            <div className="iconoClima" >
+              <img  src={"https://openweathermap.org/img/wn/" + img + "@2x.png"} width="80" height="80" alt="" />
+            </div>
       <div className="card-body">
         <Link to={`/ciudad/${id}`} >
           <h5 className="card-title">{name}</h5>
         </Link>
         <div className='details'>
-          <div className="details">
+        
             <div>
               <p>Min</p>
               <p>{min}°</p>
@@ -22,10 +25,7 @@ export default function Card({ min, max, name, img, onClose, id }) {
               <p>Max</p>
               <p>{max}°</p>
             </div>
-            <div >
-              <img className="iconoClima" src={"https://openweathermap.org/img/wn/" + img + "@2x.png"} width="80" height="80" alt="" />
-            </div>
-          </div>
+          
         </div>
 
       </div>
--- src/components/Ciudad/Ciudad.jsx
+++ src/components/Ciudad/Ciudad.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import './Ciudad.css'
 
-export default function Ciudad({city}) {
-  
-if (!city){return <h1>Ciudad no encontrada</h1>}
+export default function Ciudad({ city }) {
+
+    if (!city) { return <h1>Ciudad no encontrada</h1> }
 
     return (
         <div className="ciudad">
-                <div className="container">
-                    <h2 className='title'>{city.name}</h2>
-                    <div className="info">
-                        <div>Temperatura: {city.temp} ºC</div>
-                        <div>Clima: {city.weather}</div>
-                        <div>Viento: {city.wind} km/h</div>
-                        <div>Cantidad de nubes: {city.clouds}</div>
-                        <div>Latitud: {city.latitud}º</div>
-                        <div>Longitud: {city.longitud}º</div>
+            <div className="container">
+                <h2 className='title'>{city.name}</h2>
+                <div className="info">
+                    <div style={{textAlign:'center'}} >
+                        <img className="iconoClima" src={"https://openweathermap.org/img/wn/" + city.img + "@2x.png"} width="80" height="80" alt="" />
                     </div>
+                    <div>Temperatura: {city.temp} ºC</div>
+                    <div>Clima: {city.weather}</div>
+                    <div>Viento: {city.wind} km/h</div>
+                    <div>Cantidad de nubes: {city.clouds}</div>
+                    <div>Latitud: {city.latitud}º</div>
+                    <div>Longitud: {city.longitud}º</div>
+                </div>
             </div>
         </div>
     )
